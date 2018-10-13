016b22ad87d3761ab352d3cd96522315c627f69c
Meowcolm024 <luoyicong1026@live.com>
2018-10-13 17:38:39 +0800

    added "Quick Mode" for green card team

--- interface/interface.py
+++ interface/interface.py
@@ -4,60 +4,72 @@ from interface.Basic import *
 from interface.Support import *
 from interface.Finish import *
 from interface.Loading import *
-from util.cvs import check
+from util.cvs import analyze
 from util.log import output_log
 from config import screenshot_path
+from util.split import split
+import cv2
+
+__metaclass__ = type
 
 
-def recognize():
-    # initialize parameters
-    sh = screenshot_path
-    threshold = 0.9
-    # recognize normal interface
-    for basic_scene in basic_scenes:
-        basic_path = f"./assets/scene/{basic_scene}.png"
-        if check(sh, basic_path, threshold) == 1:
-            basic_interface = Basic()
-            basic_interface.scene = basic_path
-            basic_interface.sh = sh
-            basic_interface.get_button()
-            out = "[BASIC] Buttons: " + str(basic_interface.btn_crd)
+class Interface:
+    def __init__(self):
+        self.screenshot = cv2.imread(screenshot_path, 0)
+        self.threshold = 0.9
+        self.basic_scenes = [cv2.imread(f"./assets/scene/{basic_scene}.png", 0) for basic_scene in basic_scenes]
+        self.battle_scenes = [cv2.imread(f"./assets/battle/{battle_scene}.png", 0) for battle_scene in battle_scenes]
+        self.support_scene = cv2.imread(f"./assets/scene/{support_scene}.png", 0)
+        self.finish_scenes = [cv2.imread(f"./assets/scene/{finish_scene}.png", 0) for finish_scene in finish_scenes]
+        self.loading_scenes = [cv2.imread(f"./assets/scene/{loading_scene}.png", 0) for loading_scene in loading_scenes]
+
+    def get_basic(self):
+        for basic_scene in self.basic_scenes:
+            if analyze(self.screenshot, basic_scene, self.threshold) == 1:
+                basic_interface = Basic()
+                basic_interface.scene = basic_scene
+                basic_interface.sh = self.screenshot
+                basic_interface.get_button()
+                out = "[BASIC] Buttons: " + str(basic_interface.btn_crd)
+                print(out)
+                output_log(out)
+                return basic_interface.end
+
+    def get_support(self):
+        if analyze(self.screenshot, self.support_scene, self.threshold) == 1:
+            support_interface = Support()
+            support_interface.scene = screenshot_path
+            support_interface.select_support()
+            out = "[SUPPORT] Position: " + str(support_interface.crd)
             print(out)
             output_log(out)
-            return basic_interface.end
-    # recognize battle interface
-    for battle_scene in battle_scenes:
-        battle_path = f"./assets/battle/{battle_scene}.png"
-        if check(sh, battle_path, threshold) == 1:
-            battle_interface = Battle()
-            battle_interface.get_cards()
-            out = "[BATTLE] Cards: " + str(battle_interface.card_crd)
-            print(out)
-            output_log(out)
-            return
-    # recognize support servant interface
-    support_path = f"./assets/scene/{support_scene}.png"
-    if check(sh, support_path, threshold) == 1:
-        support_interface = Support()
-        support_interface.scene = sh
-        support_interface.select_support()
-        out = "[SUPPORT] Position: " + str(support_interface.crd)
-        print(out)
-        output_log(out)
-        return
-    # recognize finish interface
-    for finish_scene in finish_scenes:
-        finish_path = f"./assets/scene/{finish_scene}.png"
-        if check(sh, finish_path, threshold) == 1:
-            finish_interface = Finish()
-            finish_interface.scene = sh
-            finish_interface.pass_finish()
-            return
-    # recognize loading/in-battle interface
-    for loading_scene in loading_scenes:
-        loading_path = f"./assets/scene/{loading_scene}.png"
-        if check(sh, loading_path, threshold) == 1:
-            loading_interface = Loading()
-            loading_interface.scene = sh
-            loading_interface.mark = loading_scene
-            loading_interface.have_fun()
+
+    def get_finish(self):
+        for finish_scene in self.finish_scenes:
+            if analyze(self.screenshot, finish_scene, self.threshold) == 1:
+                finish_interface = Finish()
+                finish_interface.scene = screenshot_path
+                finish_interface.pass_finish()
+
+    def get_loading(self):
+        for loading_scene in self.loading_scenes:
+            if analyze(self.screenshot, loading_scene, self.threshold) == 1:
+                loading_interface = Loading()
+                loading_interface.scene = screenshot_path
+                loading_interface.mark = self.loading_scenes.index(loading_scene)
+                loading_interface.have_fun()
+
+    def get_battle(self):
+        for battle_scene in self.battle_scenes:
+            if analyze(self.screenshot, battle_scene, self.threshold) == 1:
+                split()
+                combat = Battle()
+                combat.get_cards()
+                break
+
+    def recognize(self):
+        self.get_basic()
+        self.get_support()
+        self.get_battle()
+        self.get_finish()
+        self.get_loading()
